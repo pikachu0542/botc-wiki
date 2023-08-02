9495627c9d440711731b6539527c9609a3f454ee
Will Nilges <wdn5796@rit.edu>
2023-08-02 15:33:48 -0400

    Oops remove commented stuff

--- download_git_extensions.sh
+++ download_git_extensions.sh
@@ -1,20 +1,6 @@
-#wget https://github.com/Telshin/Spoilers/archive/master.zip; \
-#unzip master.zip; \
-#mv Spoilers-master /var/www/html/extensions/Spoilers; \
-#rm master.zip; \
-#wget https://github.com/Pavelovich/WikiBanner/archive/master.zip; \
-#unzip master.zip; \
-#mv WikiBanner-master /var/www/html/extensions/WikiBanner; \
-#rm master.zip; \
-#wget https://github.com/wikimedia/mediawiki-extensions-OpenIDConnect/archive/refs/heads/master.zip; \
-#unzip master.zip; \
-#mv mediawiki-extensions-OpenIDConnect-master /var/www/html/extensions/OpenIDConnect; \
-#rm master.zip; \
-#wget https://github.com/WillNilges/mediawiki-aws-s3/archive/refs/heads/master.zip; \
-#unzip master.zip; \
-#mv mediawiki-aws-s3-master /var/www/html/extensions/AWS; \
-#rm master.zip; 
-
+#!/bin/bash
+set -e
+# You can specify extensions to be downloaded via GitHub here. You MUST specify a target folder below as well, or shit will break :)
 extensions=( Telshin/Spoilers Pavelovich/WikiBanner wikimedia/mediawiki-extensions-OpenIDConnect WillNilges/mediawiki-aws-s3 )
 targets=( Spoilers WikiBanner OpenIDConnect AWS )
 
@@ -27,7 +13,8 @@ for i in "${!extensions[@]}"; do
     rm master.zip
 
     unzipped=${extension#*/}"-master"
-    echo Moving $unzipped to extensions folder...
-    mv $unzipped /var/www/html/extensions/$target
+    target_dir=/var/www/html/extensions/$target/
+    echo Moving $unzipped to $target_dir ...
+    mv $unzipped $target_dir
     echo
 done
